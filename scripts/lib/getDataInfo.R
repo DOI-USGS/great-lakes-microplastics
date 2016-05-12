31b098208d4fe3eb7df4f810c23d7b04f3907b16
Alison Appling <aappling@usgs.gov>
2016-05-12 17:52:20 -0500

    tentative run_scripts.yaml and getDataInfo()

---
+++ scripts/lib/getDataInfo.R
@@ -0,0 +1,8 @@
+getDataInfo <- function() {
+  # read viz.yaml
+  viz.yaml <- yaml.load_file('viz.yaml')
+  
+  # isolate the data block
+  
+  # return a data.frame with columns for id, sbid, and sbfile, one row per file
+}
