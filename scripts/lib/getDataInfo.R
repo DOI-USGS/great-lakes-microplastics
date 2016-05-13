35746dcb67db7eb776a1669dfd417f6554df7b2c
Alison Appling <aappling@usgs.gov>
2016-05-13 14:24:27 -0500

    don't need getDataInfo after all

--- scripts/lib/getDataInfo.R
+++
@@ -1,8 +0,0 @@
-getDataInfo <- function() {
-  # read viz.yaml
-  viz.yaml <- yaml.load_file('viz.yaml')
-  
-  # isolate the data block
-  
-  # return a data.frame with columns for id, sbid, and sbfile, one row per file
-}
