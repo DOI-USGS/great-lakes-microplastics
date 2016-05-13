6ac6d52e8e2ec729cfbb317c85b5b0607fa62040
Laura DeCicco <ldecicco@usgs.gov>
2016-05-13 16:44:09 -0500

    debug data.yaml

--- scripts/1_munge/mungeLandUse.R
+++ scripts/1_munge/mungeLandUse.R
@@ -1,19 +1,8 @@
 #' @import data.table
 #' @import dplyr
 #' @import reshape2
-#' @import readxl
 
-mungeLandUse <- function(fname){
-
-  raw.data <- fread(fname[1])
-  raw.data <- setDF(raw.data)
-  
-  SI.1 <- read_excel(fname[2], skip = 2)
-  land.per.cols <- which(is.na(names(SI.1)))
-  land.per.cols <- c(land.per.cols[1]-1, land.per.cols)
-  names(SI.1)[land.per.cols] <- SI.1[1,land.per.cols]
-  SI.1 <- SI.1[-1,]
-  SI.1[,land.per.cols] <- sapply(SI.1[,land.per.cols], function(x) as.numeric(x))
+mungeLandUse <- function(raw.data, target_name){
 
   # ignore size classes for now, just look at totals
   allSizes <- filter(raw.data, sizeCategory == "total_all")
@@ -37,9 +26,8 @@ mungeLandUse <- function(fname){
   
   # convert to long
   conc.summary <- melt(siteAvg, id.vars=c("shortName", "UrbanPct", "OtherPct", "AgTotalPct"), variable.name="type", value.name="conc_per_m3")
-  
-  f.path <- "cache/mungeLandUse.tsv"
-  write.table(conc.summary,file=f.path, sep="\t")
+
+  write.table(conc.summary,file=target_name, sep="\t")
   return(f.path)
 }
 
