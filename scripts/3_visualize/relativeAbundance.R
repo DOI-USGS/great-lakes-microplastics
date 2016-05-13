f2024564df77be66d4d1cbcbed2a526b86756551
Jordan S Read <jread@usgs.gov>
2016-05-13 11:42:30 -0500

    running abundance w/ real data

--- scripts/3_visualize/relativeAbundance.R
+++ scripts/3_visualize/relativeAbundance.R
@@ -1,22 +1,26 @@
 
 #' @import dinosvg
+#' @import dplyr
 #' 
-visualizeRelativeAbundance <- function(tag='desktop'){
+visualizeRelativeAbundance <- function(tag='desktop', data.in){
   svg <- dinosvg:::init_svg(width = 12, height = 6)
   
-  groups <- list(list(col='#7fc97f', perc=21.2, cx='50', cy='50', name='FOAM'),
-                 list(col='#beaed4', perc=11.9, cx='150', cy='50', name='FILM'),
-                 list(col='#fdc086', perc=30.7, cx='250', cy='50', name='PELLET/BEAD'),
-                 list(col='#ffff99', perc=4.3, cx='350', cy='50', name='FRAGMENT'),
-                 '5'=list(col='#386cb0', perc=31.9, cx='450', cy='50', name='FIBER/LINE'))
+  groups <- list(list(col='#7fc97f', cx='50', cy='50', name='Foam'),
+                 list(col='#beaed4', cx='150', cy='50', name='Film'),
+                 list(col='#fdc086', cx='250', cy='50', name='Pellet/Bead'),
+                 list(col='#ffff99', cx='350', cy='50', name='Fragment'),
+                 '5'=list(col='#386cb0', cx='450', cy='50', name='Fiber/Line'))
   
   start.y <- 150
   bar.height <- 250
   for (group in groups){
-    height <- group$perc*bar.height/100
+    name <- group$name
+    perc <- filter(data.in, Figure.Name==name) %>% .$Percent.Type
+    height <- perc*bar.height/100
     fill <- group$col
     cx <- group$cx
     cy <- group$cy
+    
     g <- dinosvg:::svg_node("g", svg, c(id=group$name))
     dinosvg:::svg_node("rect", g, c(x="200", y=start.y, width="100", height=height, fill=fill))
     dinosvg:::svg_node("circle", g, c(cx=cx, cy=cy,r='40', fill=fill))
