51e7b99cd1c031f6ad14eb415d2996ab53cd56e6
Laura DeCicco <ldecicco@usgs.gov>
2016-05-16 14:27:13 -0500

    Edits to site yaml

--- scripts/3_visualize/relativeAbundance.R
+++ scripts/3_visualize/relativeAbundance.R
@@ -17,17 +17,17 @@ visualizeRelativeAbundance <- function(tag='desktop', file.in, file.text, target
 
 # The workhorse function
 
-    data.in <- read.table(file.in, header = TRUE, sep = '\t', stringsAsFactors = FALSE)
+  data.in <- read.table(file.in, header = TRUE, sep = '\t', stringsAsFactors = FALSE)
   
   text.in <- yaml.load_file(file.text)
   
   svg <- dinosvg:::init_svg(width = 12, height = 6)
   
-  groups <- list(list(col='#7fc97f', cx='50', cy='50', name='Foam'),
-                 list(col='#beaed4', cx='150', cy='50', name='Film'),
-                 list(col='#fdc086', cx='250', cy='50', name='Pellet/Bead'),
-                 list(col='#ffff99', cx='350', cy='50', name='Fragment'),
-                 '5'=list(col='#386cb0', cx='450', cy='50', name='Fiber/Line'))
+  groups <- list(list(col='#7fc97f', cx='50', cy='50', name='foams'),
+                 list(col='#beaed4', cx='150', cy='50', name='films'),
+                 list(col='#fdc086', cx='250', cy='50', name='pellets'),
+                 list(col='#ffff99', cx='350', cy='50', name='fragments'),
+                 '5'=list(col='#386cb0', cx='450', cy='50', name='fiberlines'))
   
   start.y <- 150
   bar.height <- 250
@@ -43,9 +43,9 @@ visualizeRelativeAbundance <- function(tag='desktop', file.in, file.text, target
     dinosvg:::svg_node("rect", g, c(x="200", y=start.y, width="100", height=height, fill=fill))
     dinosvg:::svg_node("circle", g, c(cx=cx, cy=cy,r='40', fill=fill))
     dinosvg:::svg_node("text", g, c(x=cx, y=cy,'text-anchor'='middle',dy='0.33em'),
-                       newXMLTextNode("Bead"))
-    dinosvg:::svg_node("text", g, c(x=cx, y=cy,'text-anchor'='middle',dy='0.33em', opacity='0.2',id="something"),
-                       newXMLTextNode("Detail 1"))
+                       newXMLTextNode(text.in[paste0("relAbundance-",group$name,"-label")]))
+    dinosvg:::svg_node("text", g, c(x=cx, y=cy,'text-anchor'='middle',dy='0.33em', opacity='0.2',id=paste0(group$name,".details")),
+                       newXMLTextNode(text.in[paste0("relAbundance-",group$name)]))
     
     start.y <- start.y+height
   }
