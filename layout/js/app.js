a4a239611e9871873377cfecedeea36b804e1192
Wernimont, Martin R <mwernimont@usgs.gov>
2016-05-27 12:43:29 -0500

    figure 1 tweak and removing accordion code

--- layout/js/app.js
+++ layout/js/app.js
@@ -1,29 +1,5 @@
 $(document).ready(function(){
 
-//Accordion
-function close_accordion_section(){
-	$('.accordion .accordion-section-title').removeClass('active');
-	$('.accordion .accordion-section-content').slideUp(300).removeClass('open');
-}
-
-$('.accordion-section-title').on('click', function(e){
-	// Grab current anchor value
-    var currentAttrValue = $(this).attr('href');
-	
-	if($(e.target).is('.active')) {
-		close_accordion_section();
-	}else {
-     	close_accordion_section();
-		
-		// Add active class to section title
-        $(this).addClass('active');
-        // Open up the hidden content panel
-        $('.accordion ' + currentAttrValue).slideDown(300).addClass('open'); 
-	}
-	
-	e.preventDefault();
-});
-
 // init controller
 var controller = new ScrollMagic.Controller();
 var triggers = {};
